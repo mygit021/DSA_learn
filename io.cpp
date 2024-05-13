21d930ffa675b6d5196abac0a0a8c67d0a9f74eb
mygit021 <21ee02004@iitbbs.ac.in>
2024-05-13 11:55:58 +0530

    added condition if else

--- io.cpp
+++ io.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
-#include <vector>
-#include <string>
 
 using namespace std;
 
-int main()
-{
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+int main(){
+    int x, y, z;
+    cout << "Enter the value of x:";
+    cin >> x >> y >> z;
+    cout << "Value of x: " << x << endl;
+    cout << "Value of y: " << y << endl;
+    cout << "Value of z: " << z << endl;
+    cout << "hello, World" << endl <<"\n";
+    cout << "hello harsh how are you";
+    return 0;
 }
